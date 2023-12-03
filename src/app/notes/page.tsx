dc59253dc5e0cfdbc5aa39883a9ba62c53ab1a79
sbordov <stefan.bordovsky@gmail.com>
2023-12-02 21:22:58 -0600

    Commenting notes to fix build broken by eslint.

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-        <h1>Quick page creation:</h1>
+        {/* <h1>Quick page creation:</h1>
         <body>Create a new .tsx file and type rafce + hit enter to autogenerate a new page.</body>
         <h1>Client-side navigation</h1>
         <body>Use Link library component instead of anchors for client-side nav.</body>
         <h1>Client-side rendering (CSR) vs Server-side rendering (SSR)</h1>
         <body>Client side rendering requires large bundles loaded on clients. They are resource intensive. Also less secure bc info available client-side. SSR: smaller bundles, more resource efficient, search-index optimized. SSR components can't listen to browser events, access browser APIs, maintain state, or use effects. Only use CSR when absolutely needed. Can extract tiny components to client to minimize CSR bits and maximize SSR.</body>
         <body>// the below directive includes the ProductCard in the CSR component bundle
-'use client';</body>
+'use client';</body> */}
     </div>
   )
 }
